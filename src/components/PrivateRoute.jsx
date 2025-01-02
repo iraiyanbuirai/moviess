4a3dd48615bedea0eba56f45b7344d56042b76ee
vcode622 <aravindram@vcodewonders.com>
2025-01-02 11:37:27 +0530

    a

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -3,10 +3,13 @@ import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
   const userToken = localStorage.getItem("user_token");
+  
+  // If no token is found, redirect to login and prevent going back to the private route
   if (!userToken) {
-    return <Navigate to="/login" />;
+    return <Navigate to="/login" replace />;
   }
 
+  // If user is authenticated, render the child components
   return children;
 };
 
